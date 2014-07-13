f9b29ce04ebb13cc1642beaf41a408e6a70ad65b
antonioldg <antonioldg@hotmail.com>
2014-07-13 23:09:08 +0200

    Take out legend box
    Take out legend box and take out (kilowatts) text from second plot ylab

--- plot4.R
+++ plot4.R
@@ -17,7 +17,7 @@ png(filename = "plot4.png",width = 480,height = 480)
 par(mfrow=c(2,2))
 ##plot2
 plot(datos$Datetime,datos$Global_active_power,type="l",xlab="",ylab="Global 
-Active Power(kilowatts)")
+Active Power")
 ##plot 1,2
 plot(datos$Datetime,datos$Voltage,type="l",xlab="datetime",ylab="Voltage")
 ##plot 2,1
@@ -26,7 +26,7 @@ points(datos$Datetime,datos$Sub_metering_1,col="black",type="l")
 points(datos$Datetime,datos$Sub_metering_2,col="red",type="l")
 points(datos$Datetime,datos$Sub_metering_3,col="blue",type="l")
 
-legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"))
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"),bty = "n")
 ##plot2,2
 plot(datos$Datetime,datos$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
 
